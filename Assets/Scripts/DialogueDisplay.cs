60d5e956b1028ff8a11e5d9f1ced11b85dcd9c94
davidfan168 <davidfan168@hotmail.com>
2022-06-24 18:24:36 -0400

    title script

---
+++ Assets/Scripts/DialogueDisplay.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueDisplay : MonoBehaviour
+{
+    public float textInterval;
+    
+    private float timeElapsed;
+    
+    void Start()
+    {
+        timeElapsed = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DisplayText(string text)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneManagementScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagementScript: MonoBehaviour
+{
+    public static void ToMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
